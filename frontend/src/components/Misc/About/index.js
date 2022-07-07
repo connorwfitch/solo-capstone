e67f8970b62c779459d88fa19798af58ee5d7cbc
connorwfitch <connorwfitch@gmail.com>
2022-07-07 10:22:42 -0400

    about page done

--- frontend/src/components/Misc/About/index.js
+++ frontend/src/components/Misc/About/index.js
@@ -1,22 +1,47 @@
-// External modules
-import { Link } from "react-router-dom";
+// Internal modules
+import SplashNav from "../../Splash/SplashNav";
+import Footer from "../Footer";
+import './About.css'
 
 function About() {
   return (
-    <div>
-      <a href="https://github.com/connorwfitch/react-solo-project">
-        Project Repo
-      </a>
-      <a href="https://github.com/connorwfitch">
-        My GitHub
-      </a>
-      <a href="https://www.linkedin.com/in/connor-fitch-241678159/">
-        My LinkedIn
-      </a>
-      <Link to='/'>
-        Return to home page
-      </Link>
-    </div>
+    <>
+      <SplashNav />
+      <div className="about-main">
+        <h1>About</h1>
+        <div className="about-text">
+          <p className="light">
+            Hi, I am Connor Fitch, the developer for this website. I use Todoist on
+            a daily basis to help me stay organized and productive.
+          </p>
+          <p className="light">
+            Given my familiarity with the product and my admiration for their UI/UX design,
+            I knew I wanted to try my hand at making my own version of it. I hope you enjoy!
+          </p>
+        </div>
+        
+        <div className="about-links">
+          <a href="https://github.com/connorwfitch/react-solo-project" className="link"
+            target='_blank' rel='noreferrer'>
+            Project Repo
+          </a>
+          <a href="https://github.com/connorwfitch" className="link"
+            target='_blank' rel='noreferrer'>
+            My GitHub
+          </a>
+          <a href="https://www.linkedin.com/in/connor-fitch-241678159/" className="link"
+            target='_blank' rel='noreferrer'>
+            My LinkedIn
+          </a>
+          <a href="https://todoist.com/" className="link"
+            target='_blank' rel='noreferrer'>
+            Todoist
+          </a>
+        </div>
+        <img src="/images/cards.png" className='about-img' alt='productive people' />
+      </div>
+      <Footer />
+    </>
   )
 }
 
