19a2e62cffb552826a871e42d4c32c221773826c
connorwfitch <connorwfitch@gmail.com>
2022-08-03 16:05:23 -0400

    hooked up phase 2 api routes to the api

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,6 +6,9 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const listsRouter = require('./lists.js');
 const tasksRouter = require('./tasks.js');
+const boardsRouter = require('./boards.js');
+const sectionsRouter = require('./sections.js');
+const itemsRouter = require('./items.js');
 
 // Creating api router
 const router = express.Router();
@@ -17,5 +20,8 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/lists', listsRouter);
 router.use('/tasks', tasksRouter);
+router.use('/boards', boardsRouter);
+router.use('/sections', sectionsRouter);
+router.use('/items', itemsRouter);
 
 module.exports = router;
