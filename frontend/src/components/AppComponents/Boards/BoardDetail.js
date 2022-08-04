080c0979c2bd75ce9f19d4bd29930ff9edf032a7
connorwfitch <connorwfitch@gmail.com>
2022-08-04 12:18:55 -0400

    changing react version for dnd compatibility

---
+++ frontend/src/components/AppComponents/Boards/BoardDetail.js
@@ -0,0 +1,11 @@
+// External modules
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { DragDropContext } from 'react-beautiful-dnd';
+
+// Internal modules
+
+function BoardDetail({ showSidebar }) {
+
+}
