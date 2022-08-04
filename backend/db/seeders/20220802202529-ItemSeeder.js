1e3a8614277ac04995c7544c46962991612efc6f
connorwfitch <connorwfitch@gmail.com>
2022-08-04 16:46:56 -0400

    seeds for new inexing method

--- backend/db/seeders/20220802202529-ItemSeeder.js
+++ backend/db/seeders/20220802202529-ItemSeeder.js
@@ -6,34 +6,28 @@ module.exports = {
       // Board 1
       {
         title: 'Synergize team infrastructure',
-        index: 1,
         sectionId: 1
       },
       {
         title: 'Onboard new multi-channel paradigm',
-        index: 2,
         sectionId: 1
       },
       {
         title: 'Streamline operations',
-        index: 3,
         sectionId: 1
       },
 
       // Board 2
       {
         title: 'Secure funding for West Dakota',
-        index: 1,
         sectionId: 4
       },
       {
         title: 'Create West Dakota',
-        index: 2,
         sectionId: 4
       },
       {
         title: 'Profit $$$',
-        index: 3,
         sectionId: 4
       },
 
