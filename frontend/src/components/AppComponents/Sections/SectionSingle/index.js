6b65c9c0cacd7a4f31196f9f8094bf6ffa9c26a0
connorwfitch <connorwfitch@gmail.com>
2022-08-09 12:53:28 -0400

    stopping for dsa work with stili and chase

--- frontend/src/components/AppComponents/Sections/SectionSingle/index.js
+++ frontend/src/components/AppComponents/Sections/SectionSingle/index.js
@@ -5,12 +5,44 @@ import ItemSingle from "../../Items/ItemSingle";
 
 // Internal modules
 
-function SectionSingle({ section, index }) {
+function SectionSingle({ section, index, showMenu, setShowMenu, showEditor, setShowEditor }) {
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    // return dispatch(deleteTask(task.id))
+  }
+
+  let buttons;
+  if ('section-' + section.id === showMenu) {
+    buttons = (
+      <div className="section-single-menu">
+        <button className="section-single-actions" onClick={handleDelete}>
+          <i className="fa-solid fa-trash"></i>
+        </button>
+        <button className="section-single-actions" onClick={() => setShowEditor('section-' + section.id)}>
+          <i className="fa-solid fa-pen"></i>
+        </button>
+        <button className="section-single-actions" onClick={() => setShowMenu('')}>
+          <i className="fa-solid fa-ellipsis"></i>
+        </button>
+      </div>
+    )
+  } else {
+    buttons = (
+      <button className="section-single-actions" onClick={() => setShowMenu('section-' + section.id)}>
+        <i className="fa-solid fa-ellipsis"></i>
+      </button>
+    )
+  }
+
   return (
     <div className="section-single">
-      <p className="section-title">
-        {section.title}
-      </p>
+      <div className="section-header">
+        <p className="section-title">
+          {section.title}
+        </p>
+        {buttons}
+      </div>
       <Droppable droppableId={'section-' + section.id}>
         {(provided, snapshot) => {
           let className = "items-holder";
